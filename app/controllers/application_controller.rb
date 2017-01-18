93f513d91242a8d91030a312176f41675d127547
[tishpr] <[tishsny@gmail.com]>
2017-01-18 13:34:53 -0800

    set up routes, not working yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
     def hello
-    render html: "Hello, world!"
+    render html: "Hello, world!"  
   end
   def bye
     render html: "Until next time!"
   end
 end
+
+ # eU8ZzVUAFoEg3
+ 
