08923ae63df351f9c81d734382595ccfc6ea8b92
[tishpr] <[tishsny@gmail.com]>
2017-01-17 18:40:18 -0800

    add new route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+    def hello
+    render html: "Hello, world!"
+  end
+  def bye
+    render html: "Until next time!"
+  end
 end
